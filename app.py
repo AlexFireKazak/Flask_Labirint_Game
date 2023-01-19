b6668a89ef5ec22ae8021ef5ec0d7c08b3906635
AlexFireKazak <alexfirekazak@gmail.com>
2023-01-19 11:52:57 +0300

    doing pre-second-page

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ def index():
 
     return render_template("index.html")
 
+@app.route("/game/")
+def game():
+
+    return render_template("game.html")
+
 if __name__ =='__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
